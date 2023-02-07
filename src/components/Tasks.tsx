c2c18a330452794dd31c237b02b0072a3bf07dce
João Mangueira <jamjs7@gmail.com>
2023-02-07 11:58:13 -0300

    add checkbox

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,7 +1,7 @@
-import { Trash } from "phosphor-react";
+import { useState } from "react";
+import { Trash, Check } from "phosphor-react";
 import styles from "./Tasks.module.css";
 import { TasksProps } from "../App";
-import {useState} from "react";
 
 
 export function Tasks({ id, description, done } : TasksProps ) {
@@ -13,14 +13,24 @@ export function Tasks({ id, description, done } : TasksProps ) {
 
   return(
     <div className={styles.container}>
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={handleOnChangeDoneCheckbox}
-        id={id.toString()}
-        name="tasks"
-        value={id}
-      />
+      <label className={styles.checkbox}>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleOnChangeDoneCheckbox}
+          id={id.toString()}
+          name="tasks"
+          value={id}
+        />
+        <span className={styles.checked}>
+          {
+            checked ? <Check
+                      size={14}
+                      color="#F2F2F2"
+                    /> : ""
+          }
+        </span>
+      </label>
       <span className={styles.description}>{description}</span>
       <Trash
         size={24}
